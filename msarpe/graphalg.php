005086d75b4584dba5d4cc081a3cf709e5c6b786
LINDVIOR2 <sarpe36@gmail.com>
2018-12-05 14:30:51 +0200

    need mor changes

--- msarpe/graphalg.php
+++ msarpe/graphalg.php
@@ -1,5 +1,25 @@
 <?php
 $string = file_get_contents("nw.json");
-$json_a = json_decode($string, true);
+$data = json_decode($string, true);
 
-echo $json_a($string);
+echo $data($string);
+
+class Dijkstra
+{
+    /** @var integer[][] The graph, where $graph[node1][node2]=cost */
+    protected $graph;
+    /** @var int[] Distances from the source node to each other node */
+    protected $distance;
+    /** @var string[][] The previous node(s) in the path to the current node */
+    protected $previous;
+    /** @var int[] Nodes which have yet to be processed */
+    protected $queue;
+
+    /**
+     * @param integer[][] $graph
+     */
+    public function __construct($graph)
+    {
+        $this->graph = $graph;
+    }
+}
