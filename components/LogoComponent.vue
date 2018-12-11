8188bd1cbf11dc1a38990fad4e0c62b96b0dfc57
aignatiuc <arina.ignatiuc@ee.utm.md>
2018-12-11 14:52:31 +0200

    removed unneeded files

--- components/LogoComponent.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <img src="/pentalog.png" class="header_logo">
-</template>
--- plugins/vue-konva.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-import VueKonva from 'vue-konva'
-
-export default () => {
-  Vue.use(VueKonva)
-}
